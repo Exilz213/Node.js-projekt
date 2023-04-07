14d21e0e6583b39acad30675349b16afb0f975c1
momed <mohammed.abdu@elev.ga.lbs.se>
2023-04-07 12:45:47 +0200

    Thrid commit

--- upload.js
+++ upload.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
    storage: storage,
-   limits: {fileSize: 10*1024*1024},
+   limits: {fileSize: 50*1024*1024},
    fileFilter: function(req, file, cb){
        checkFileType(file, cb);
    }
